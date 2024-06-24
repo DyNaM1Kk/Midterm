9e40552d51a7c9223f0a1ce26a6d563ffbf8f92e
PC <PC@DESKTOP-7450GMH>
2024-06-24 14:26:14 +0400

    Add project files.

---
+++ DTOs/BookDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Midterm.DTOs
+{
+    public class BookDTO
+    {
+        [Required]
+        [MinLength(4)]
+        [MaxLength(32)]
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime PublishedDate { get; set; }
+        [Required]
+        [MinLength(4)]
+        [MaxLength(24)]
+        public string? Author { get; set; }
+    }
+}
